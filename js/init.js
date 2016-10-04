e4b8121450bcaee939a4a29f787dc49c3dd87751
Julioocz <julioocz@gmail.com>
2016-10-03 22:56:50 -0400

    Initial commit of my website

---
+++ js/init.js
@@ -0,0 +1,50 @@
+(function($) {
+    $(function() {
+
+        $('.button-collapse').sideNav();
+
+        // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+
+                // Store hash
+                var hash = this.hash;
+
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function() {
+
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            } // End if
+        }); // end of document ready
+    })
+})(jQuery); // end of jQuery name space
+
+var options = [{
+        selector: '#skills',
+        offset: 250,
+        callback: function(el) {
+            $(el).addClass("animated slideInLeft");
+            $(el).removeClass("cantsee")
+        }
+    },
+    {
+        selector: '#aboutMe',
+        offset: 250,
+        callback: function(el) {
+            $(el).addClass("animated slideInRight");
+            $(el).removeClass("cantsee")
+        }
+    }
+];
+
+
+Materialize.scrollFire(options);
